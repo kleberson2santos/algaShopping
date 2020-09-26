b5c47f5929aa8638c4d2de9bc18dea986f2e9edb
Kleberson Santos <kleberson2santos@gmail.com>
2020-09-26 12:06:02 -0400

    add base layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import AppHeader from '../AppHeader'
+import AppContainer from '../AppContainer'
 import { Wrapper, Container } from './App.styles'
 
 function App (){
     return <Wrapper>
-        <Container>todo: app</Container>
+        <Container>
+            <AppHeader />
+            <AppContainer 
+             left={<div style={{backgroundColor: 'red'}}>
+                 produtos disponiveis
+             </div>}
+             middle={<div style={{backgroundColor: 'green'}}>
+                 sua lista de compras
+             </div>
+             }
+             right={<div style={{backgroundColor: 'blue'}}>
+                 estatisticas
+             </div>}
+            />
+        </Container>
         </Wrapper>
 }
 
