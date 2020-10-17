f4e739f7933639475c5eb81bd7caee453a4d9ea3
Kleberson Santos <kleberson2santos@gmail.com>
2020-10-16 20:38:26 -0400

    Finish the base logic

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AppHeader from '../AppHeader'
 import AppContainer from '../AppContainer'
 import { Wrapper, Container } from './App.styles'
 import LineChart from '../../shared/LineChart'
 import ShoppingList from '../ShoppingList'
 import productsMock from '../../mocks/productsList.json'
-import { useEffect } from 'react'
+import extractPercentage from '../../utils/extractPercentage'
 
 function App() {
 
@@ -13,18 +13,28 @@ function App() {
 
     const [products, setProducts] = useState(productsMock.products)
     const [selectedProducts, setSelectedProduct] = useState([])
+    const [totalPrice, setTotalPrice] = useState(0)
 
-    useEffect(()=> {
+    useEffect(() => {
         const newSelectedProducts = products
-        .filter(product=> product.checked)
+            .filter(product => product.checked)
         setSelectedProduct(newSelectedProducts)
     }, [products])
-    
-    function handleToggle (id, checked, name) {
-        const newProducts = products.map(product => product.id === id 
-            ? {...product, checked: !product.checked} 
+
+    useEffect(()=>{
+        const total =selectedProducts
+        .map(product => product.price)
+        .reduce((a,b) => a+b, 0)
+
+        setTotalPrice(total)
+
+    }, [selectedProducts])
+
+    function handleToggle(id, checked, name) {
+        const newProducts = products.map(product => product.id === id
+            ? { ...product, checked: !product.checked }
             : product
-           
+
         )
         setProducts(newProducts)
     }
@@ -35,19 +45,56 @@ function App() {
             <AppContainer
                 left={
                     <ShoppingList
-                        title="Produtos disponíveis" 
+                        title="Produtos disponíveis"
                         products={products}
-                        onToggle={handleToggle}/>}
+                        onToggle={handleToggle} />}
                 middle={
                     <ShoppingList
-                        title="Sua lista de compras" 
-                        products={selectedProducts}/>}
+                        title="Sua lista de compras"
+                        products={selectedProducts} />}
                 right={<div>
                     estatisticas:
-                 <LineChart title="saudável" color={colors[0]} percentage={80} />
-                    <LineChart title="não tão saudável" color={colors[1]} percentage={20} />
-                    <LineChart title="limpeza" color={colors[2]} percentage={35} />
-                    <LineChart title="outros" color={colors[3]} percentage={20} />
+                    <LineChart 
+                     title="saudável" 
+                     color={colors[0]} 
+                     percentage={extractPercentage(
+                         selectedProducts.length,
+                         selectedProducts.filter(product=>product.tags.includes('healthy'))
+                         .length)} />
+                    <LineChart 
+                     title="não tão saudável" 
+                     color={colors[1]}
+                     percentage={extractPercentage(
+                         selectedProducts.length,
+                         selectedProducts.filter(product=>product.tags.includes('junk'))
+                         .length)} />
+                    <LineChart 
+                     title="limpeza" 
+                     color={colors[2]} 
+                     percentage={extractPercentage(
+                        selectedProducts.length,
+                        selectedProducts.filter(product=>product.tags.includes('cleaning'))
+                        .length)} />
+                    <LineChart 
+                     title="outros" 
+                     color={colors[3]} 
+                     percentage={extractPercentage(
+                        selectedProducts.length,
+                        selectedProducts.filter(product=>product.tags.includes('others'))
+                        .length)} />
+
+                    <div style={{ marginTop: 12}}>
+                        <h2 style={{ fontWeight:400, fontSize: 12, color:'#00364A' }}>
+                            previsão de gastos:
+                        </h2>
+                        <div style={{ fontSize: 24 }}>
+                            {totalPrice.toLocaleString('pt-br', {
+                                minimumFractionDigits: 2,
+                                style: 'currency',
+                                currency: 'BRL'
+                            })}
+                        </div>
+                    </div>
                 </div>}
             />
         </Container>
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -10,7 +10,7 @@ function ShoppingList ({title, products, onToggle}) {
         <Array>
             {
                 products.map(product => 
-                 <Checkbox 
+                 <Checkbox key={product.id}
                     value={product.checked} 
                     title={product.name} 
                     onClick={() => onToggle(product.id, product.checked, product.name)}/>
